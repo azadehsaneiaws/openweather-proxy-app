dbef3fca441b1bed81b213668b7b8d90f4e564a9
RoxSanei <rox.sanei@zincgroup.com>
2025-02-03 00:26:39 +1100

    ✨ Setup: Added WeatherModel and IWeatherRepository following Repository Pattern

---
+++ OpenWeatherProxyApp.Server/Models/WeatherModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenWeatherProxyApp.Server.Models
+{
+    public class WeatherModel
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+    }
+}
---
+++ OpenWeatherProxyApp.Server/Repositories/IWeatherRepository.cs
@@ -0,0 +1,10 @@
+﻿using OpenWeatherProxyApp.Server.Models;
+
+namespace OpenWeatherProxyApp.Server.Repositories
+{
+    public interface IWeatherRepository
+    {
+        Task<WeatherModel> GetWeatherAsync(string city, string country);
+
+    }
+}
